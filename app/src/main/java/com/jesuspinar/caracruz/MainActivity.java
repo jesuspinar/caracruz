30e594e3e1b50cdbc53ee230716045ce8a301c68
Jesus <jesuspl@tutanota.com>
2022-09-23 19:51:25 +0200

    Adding app icon

--- app/src/main/java/com/jesuspinar/caracruz/MainActivity.java
+++ app/src/main/java/com/jesuspinar/caracruz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jesuspinar.caracruz;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        imageView.setImageResource(R.drawable.euro_cara);
+
     }
 }
